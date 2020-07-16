c956d4ecdd8511d36b7b01a7cd704bc8c40e45f5
HAO Jiahui <jiahui_hao@126.com>
2020-07-16 10:02:41 +0800

    finish the bonus part of hw04

--- GAMES101-HW04/GAMES101-HW04/main.cpp
+++ GAMES101-HW04/GAMES101-HW04/main.cpp
@@ -4,7 +4,7 @@
 
 std::vector<cv::Point2f> control_points;
 
-const int control_point_num = 6;
+const int control_point_num = 4;
 
 void mouse_handler(int event, int x, int y, int flags, void *userdata) 
 {
@@ -14,6 +14,13 @@ void mouse_handler(int event, int x, int y, int flags, void *userdata)
         << y << ")" << '\n';
         control_points.emplace_back(x, y);
     }     
+
+    // test the Anti-aliasing method
+    // control_points.emplace_back(141, 432);
+    // control_points.emplace_back(270, 193);
+    // control_points.emplace_back(421, 200);
+    // control_points.emplace_back(300, 508);
+    // control_points.resize(4);
 }
 
 void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window) 
@@ -44,12 +51,51 @@ cv::Point2f recursive_bezier(const std::vector<cv::Point2f>& control_points, flo
     return recursive_bezier(new_control_points, t);
 }
 
+std::vector<cv::Point2f> get_points(float x, float y)
+{
+    auto x_min = std::floor(x) + 0.5f;
+    auto y_min = std::floor(y) + 0.5f;
+
+    auto x_near = x - x_min < 0.5 ? -1 : 1;
+    auto y_near = y - y_min < 0.5 ? -1 : 1;
+
+    std::vector<cv::Point2f> res;
+    res.emplace_back(cv::Point2f(x_min, y_min));
+    res.emplace_back(cv::Point2f(x_min + x_near * 1, y_min + y_near * 1));
+    res.emplace_back(cv::Point2f(x_min + x_near * 1, y_min + y_near * 0));
+    res.emplace_back(cv::Point2f(x_min + x_near * 0, y_min + y_near * 1));
+
+    return res;
+}
+
+void anti_aliasing(float x, float y, cv::Mat& window)
+{
+    cv::Point2f p(x, y);
+    auto neighbor_point = get_points(x, y);
+
+    double dis[4];
+    double min_dis = std::numeric_limits<double>::max();
+    int ind = 0;
+    for (int i = 0; i < 4; i++) {
+        dis[i] = cv::norm(p - neighbor_point[i]);
+        if (dis[i] < min_dis) {
+            min_dis = dis[i];
+            ind = i;
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        window.at<cv::Vec3b>(neighbor_point[i].y, neighbor_point[i].x)[1] = std::max(static_cast<double>(window.at<cv::Vec3b>(neighbor_point[i].y, neighbor_point[i].x)[1]), 255 * min_dis / dis[i]);
+    }
+}
+
 void bezier(const std::vector<cv::Point2f>& control_points, cv::Mat& window)
 {
-    float step = 0.001;
+    float step = 0.0001;
     for (float t = 0; t <= 1; t += step) {
         auto point = recursive_bezier(control_points, t);
         window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
+        anti_aliasing(point.x, point.y, window);
     }
 }
 
